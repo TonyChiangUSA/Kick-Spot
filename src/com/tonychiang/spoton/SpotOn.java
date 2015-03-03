8ae27ff7d98d70a70c8ffdcaed88c42213524351
TonyChiangUSA <www.gasiajang@gmail.com>
2015-03-03 08:50:46 +0800

    first commit

--- src/com/tonychiang/spoton/SpotOn.java
+++ src/com/tonychiang/spoton/SpotOn.java
@@ -1,5 +1,3 @@
-// SpotOn.java
-// Activity for the SpotOn app
 package com.tonychiang.spoton;
 
 import com.deitel.spoton.R;
@@ -11,39 +9,34 @@ import android.widget.RelativeLayout;
 
 public class SpotOn extends Activity 
 {
-   private SpotOnView view; // displays and manages the game
+   private SpotOnView view; 
    
-   // called when this Activity is first created
    @Override
    public void onCreate(Bundle savedInstanceState) 
    {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.main);
 
-      // create a new SpotOnView and add it to the RelativeLayout
       RelativeLayout layout = 
          (RelativeLayout) findViewById(R.id.relativeLayout);
       view = new SpotOnView(this, getPreferences(Context.MODE_PRIVATE), 
          layout); 
-      layout.addView(view, 0); // add view to the layout
-   } // end method onCreate
+      layout.addView(view, 0); 
+   }
    
-   // called when this Activity moves to the background
    @Override
    public void onPause()
    {
 	   super.onPause();
-	   view.pause(); // release resources held by the View
-   } // end method onPause
+	   view.pause();
+   } 
    
-   // called when this Activity is brought to the foreground
    @Override
    public void onResume()
    {
 	   super.onResume();
-	   view.resume(this); // re-initialize resources released in onPause
-   } // end method onResume
-} // end class SpotOn
-
+	   view.resume(this); 
+   }
+}
 
 
